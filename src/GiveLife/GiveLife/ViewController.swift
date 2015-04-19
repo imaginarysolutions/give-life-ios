331bf33161be9e48e7999968adffa2f2b8f93176
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2015-04-19 12:12:23 +0200

    Created an empty Single View application project.

---
+++ src/GiveLife/GiveLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiveLife
+//
+//  Created by Ivan Fabijanovic on 19/04/15.
+//  Copyright (c) 2015 Imaginary Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
